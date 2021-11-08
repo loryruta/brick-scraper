a9d99555710bb56d3bf2ade4f881846ea2997825
loryruta <loryruta23@gmail.com>
2021-11-08 21:46:53 +0000

    Remove unused files

--- api_layer.py
+++
@@ -1,9 +0,0 @@
-import bricklink
-import brickowl
-
-
-def get_orders(direction=None, status=None, filed=None):
-    return [
-        *bricklink.get_orders(direction),  # todo direction `in`
-        *brickowl.get_orders()
-    ]
